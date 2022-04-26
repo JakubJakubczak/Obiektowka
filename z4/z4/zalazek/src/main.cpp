d8547adeabb289179d320ffb3fe6029f924cfca8
Jakub <263486@student.pwr.edu.pl>
2022-04-26 23:03:53 +0200

    FINAL

--- z4/z4/zalazek/src/main.cpp
+++ z4/z4/zalazek/src/main.cpp
@@ -8,10 +8,11 @@
 using namespace std;
 
 /*
- * Tu definiujemy pozostale funkcje.
- * Lepiej jednak stworzyc dodatkowy modul
- * i tam je umiescic. Ten przyklad pokazuje
- * jedynie absolutne minimum.
+   Program wczytuje macierz transponowana oraz wektor wyrazow wolnych
+   i na podstawie wczytanych danych wyswietla uklad rownan. Wyswietla 
+   napis mowiacy czy uklad jest oznaczony czy nie jest. Jesli jest to
+   wyswietla rozwiazania ukladu, blad obliczen jako wektor i dlugosc tego
+   wektora.
  */
 
 
@@ -24,9 +25,9 @@ int main()
   cout  << endl << " Start programu " << endl << endl;
   
   
-  cin>>UklRown;
-  cout<<UklRown;
-  wyniki=UklRown.RozwUklRow();
-  UklRown.BladObl(wyniki);
+  cin>>UklRown;  // wczytanie ukladu
+  cout<<UklRown;  // wyswietlenie go
+  wyniki=UklRown.RozwUklRow(); // rozwiazanie go, wyswietlenie i podstawienie wyniku do zmiennej wyniki
+  UklRown.BladObl(wyniki); // obliczenie bledu jako wektora i jego dlugosci i wyswietlenie tych rzeczy
     
 }
